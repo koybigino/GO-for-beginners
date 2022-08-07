621d4260b613a787465dfba55e573449692a3418
koybigino <koybigino@gmail.com>
2022-08-07 22:48:39 +0100

    Channel 2

--- src/main.go
+++ src/main.go
@@ -10,15 +10,16 @@ var wg = sync.WaitGroup{}
 func main() {
 	ch := make(chan int)
 	wg.Add(2)
-
 	go func() {
 		i := <-ch
 		fmt.Println(i)
+		ch <- 27
 		wg.Done()
 	}()
 
 	go func() {
 		ch <- 42
+		fmt.Println(<-ch)
 		wg.Done()
 	}()
 
