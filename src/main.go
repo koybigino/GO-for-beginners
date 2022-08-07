267fee3c1079270cce7a4148616abb1c6e0371ae
koybigino <koybigino@gmail.com>
2022-08-07 23:20:52 +0100

    Channel 5

--- src/main.go
+++ src/main.go
@@ -11,16 +11,16 @@ func main() {
 	ch := make(chan int, 50) // takes 50 int numbers
 	wg.Add(2)
 	go func(ch <-chan int) { // send the data to the channel only
-		i := <-ch
-		fmt.Println(i)
-		i = <-ch
-		fmt.Println(i)
+		for i := range ch {
+			fmt.Println(i)
+		}
 		wg.Done()
 	}(ch)
 
 	go func(ch chan<- int) { // receive the data to the channel only
 		ch <- 42
 		ch <- 27
+		close(ch) // close the channel
 		wg.Done()
 	}(ch)
 
