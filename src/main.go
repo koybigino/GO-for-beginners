f03e7f259915da6de700ec9287c7f9d8ce429499
koybigino <koybigino@gmail.com>
2022-08-07 23:07:50 +0100

    Channel 4 :

--- src/main.go
+++ src/main.go
@@ -8,18 +8,19 @@ import (
 var wg = sync.WaitGroup{}
 
 func main() {
-	ch := make(chan int)
+	ch := make(chan int, 50) // takes 50 int numbers
 	wg.Add(2)
 	go func(ch <-chan int) { // send the data to the channel only
 		i := <-ch
 		fmt.Println(i)
-		// ch <- 27
+		i = <-ch
+		fmt.Println(i)
 		wg.Done()
 	}(ch)
 
 	go func(ch chan<- int) { // receive the data to the channel only
 		ch <- 42
-		// fmt.Println(<-ch)
+		ch <- 27
 		wg.Done()
 	}(ch)
 
